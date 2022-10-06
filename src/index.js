a947a897e8feff76da1c02670d39a1c3f5368635
yiguozhi <yiguozhi@alibaba-inc.com>
2022-10-06 13:20:03 +0800

    html-webpack-plugin&webpack-dev-server&babel-loade

--- src/index.js
+++ src/index.js
@@ -1,13 +1,4 @@
-import _ from 'lodash';
-import printMe from './print';
-
-function component () {
-  const ele = document.createElement('div');
-  const btn = document.createElement('buttom');
-  ele.innerHTML = _.join(['Hello1', 'webpack', '岂能长少年'], ' ');
-  btn.innerHTML = 'Click me and check the console';
-  btn.onclick = printMe;
-  ele.appendChild(btn)
-  return ele;
+const sum = (num1, num2) => {
+  console.log('num1 + num2 = ' + (num1 + num2));
 }
-document.body.appendChild(component())
+sum(5.10, 5);
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,48 @@
+'use strict'
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-module.exports = {
-  mode: 'development',
-  entry: {
-    index: './src/index.js',
-    print: './src/print.js',
-  },
-  devtool: 'inline-source-map',
-  devServer: {
-    static: './dist',
-  },
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-    publicPath: '/',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Development'
-    })
-  ]
 
+// CommonJS 模块
+module.exports = {
+  // "development" | "production" | "none"
+  mode: 'none',
+  // 入口文件
+  entry: './src/index.js',
+  // 输出
+  output: {
+    // 输出路径
+    path: path.join(__dirname, 'dist'),
+    // 输出文件名
+    filename: 'index.js'
+  },
+  // 插件
+  plugins: [
+    // 自动编译出html文件,在模版基础上，插入编译生成的 js 
+    new HtmlWebpackPlugin({
+      // 模版文件
+      template: path.join(__dirname, 'src', 'index.html'),
+      // 输出文件名称
+      filename: 'index.html'
+    }),
+  ],
+  // webpack-dev-server 在保存时自动编译文件，但不会自动刷新浏览器
+  devServer: {
+    // 端口
+    port: 8080,
+    // 静态文件路径
+    static: path.join(__dirname, 'dist'),
+  },
+  // 模块，对象
+  module: {
+    // 规则
+    rules: [
+      {
+        // js文件规则
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: path.join(__dirname, 'src'),
+        exclude: /node_modules/,
+      }
+    ]
+  }
 }
